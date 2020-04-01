6bd83d86925b22b5dc822b62e16081b9ffd2be09
Nils Asmussen <nils@os.inf.tu-dresden.de>
2020-04-01 12:02:41 +0200

    Added disktest program and test file system.
    The problem is that if we want to run that in our test system, we cannot
    generate a disk every time, because that requires root privileges. So,
    we don't want to use the normal file systems, because they contain all
    binares, which need to be regenerated after every change to M3. Instead,
    we pre-generate a file system with specific test data and load all apps
    in the test via boot module.

---
+++ src/apps/disktest/src/tdisk.rs
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2018, Nils Asmussen <nils@os.inf.tu-dresden.de>
+ * Economic rights: Technische Universitaet Dresden (Germany)
+ *
+ * This file is part of M3 (Microkernel-based SysteM for Heterogeneous Manycores).
+ *
+ * M3 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * M3 is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License version 2 for more details.
+ */
+
+use m3::io::{Read, Write};
+use m3::test;
+use m3::vfs::{FileRef, OpenFlags, VFS};
+
+pub fn run(t: &mut dyn test::WvTester) {
+    wv_assert_ok!(VFS::mount("/", "m3fs", "m3fs"));
+
+    wv_run_test!(t, text_files);
+    wv_run_test!(t, pat_file);
+    wv_run_test!(t, write_file);
+}
+
+fn text_files() {
+    {
+        let mut file = wv_assert_ok!(VFS::open("/test.txt", OpenFlags::R));
+        let s = wv_assert_ok!(file.read_to_string());
+        wv_assert_eq!(s, "This is a test\n");
+    }
+
+    {
+        let mut file = wv_assert_ok!(VFS::open("/test/test.txt", OpenFlags::R));
+        let s = wv_assert_ok!(file.read_to_string());
+        wv_assert_eq!(s, "This is a test\n");
+    }
+}
+
+fn pat_file() {
+    let mut file = wv_assert_ok!(VFS::open("/pat.bin", OpenFlags::R));
+    let mut buf = vec![0u8; 8 * 1024];
+
+    wv_assert_eq!(_validate_pattern_content(&mut file, &mut buf), 64 * 1024);
+}
+
+fn write_file() {
+    // create new file
+    {
+        let mut file = wv_assert_ok!(VFS::open("/newfile", OpenFlags::W | OpenFlags::CREATE | OpenFlags::TRUNC));
+        wv_assert_ok!(write!(file, "my content is {:#x}", 0x1234));
+        // ensure it's written to disk
+        wv_assert_ok!(file.sync());
+    }
+
+    // read content back
+    {
+        let mut file = wv_assert_ok!(VFS::open("/newfile", OpenFlags::R));
+        let s = wv_assert_ok!(file.read_to_string());
+        wv_assert_eq!(s, "my content is 0x1234");
+    }
+}
+
+fn _validate_pattern_content(file: &mut FileRef, mut buf: &mut [u8]) -> usize {
+    let mut pos: usize = 0;
+    loop {
+        let count = wv_assert_ok!(file.read(&mut buf));
+        if count == 0 {
+            break;
+        }
+
+        for b in buf.iter().take(count) {
+            wv_assert_eq!(
+                *b,
+                (pos & 0xFF) as u8,
+                "content wrong at offset {}: {} vs. {}",
+                pos,
+                *b,
+                (pos & 0xFF) as u8
+            );
+            pos += 1;
+        }
+    }
+    pos
+}
