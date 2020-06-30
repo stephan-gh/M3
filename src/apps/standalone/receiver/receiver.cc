09abe7acf9dfdbcb272a69778739cabedd486698
Nils Asmussen <nils@os.inf.tu-dresden.de>
2020-06-30 16:59:30 +0200

    standalone: added send/recv test.

---
+++ src/apps/standalone/receiver/receiver.cc
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2018, Nils Asmussen <nils@os.inf.tu-dresden.de>
+ * Economic rights: Technische Universitaet Dresden (Germany)
+ *
+ * This file is part of M3 (Microkernel-based SysteM for Heterogeneous Manycores).
+ *
+ * M3 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * M3 is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License version 2 for more details.
+ */
+
+#include <base/Common.h>
+#include <base/stream/Serial.h>
+#include <base/util/Util.h>
+
+#include "../assert.h"
+#include "../tcuif.h"
+
+using namespace m3;
+
+static uint8_t rbuf[256];
+
+int main() {
+    size_t size = nextlog2<sizeof(rbuf)>::val;
+    uintptr_t rbuf_addr = reinterpret_cast<uintptr_t>(rbuf);
+    kernel::TCU::config_recv(0, rbuf_addr, size, size, 1);
+
+    Serial::get() << "Hello World from receiver!\n";
+
+    uint64_t reply = 0xCAFEBABE;
+    while(1) {
+        // wait for message
+        const TCU::Message *rmsg;
+        while((rmsg = kernel::TCU::fetch_msg(0, rbuf_addr)) == nullptr)
+            ;
+        const uint64_t *data = reinterpret_cast<const uint64_t*>(rmsg->data);
+        Serial::get() << "Got message: "
+            << "label=" << fmt(rmsg->label, "#x")
+            << ", payload=" << fmt(data[0], "#x") << "\n";
+        // const uint64_t *words = reinterpret_cast<const uint64_t*>(rmsg);
+        // for(size_t i = 0; i < 8; ++i)
+        //     Serial::get() << "word" << i << ": " << fmt(words[i], "#x") << "\n";
+
+        // send reply
+        ASSERT_EQ(kernel::TCU::reply(0, &reply, sizeof(reply), rbuf_addr, rmsg), Errors::NONE);
+        reply++;
+    }
+    return 0;
+}
---
+++ src/apps/standalone/sender/sender.cc
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2018, Nils Asmussen <nils@os.inf.tu-dresden.de>
+ * Economic rights: Technische Universitaet Dresden (Germany)
+ *
+ * This file is part of M3 (Microkernel-based SysteM for Heterogeneous Manycores).
+ *
+ * M3 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * M3 is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License version 2 for more details.
+ */
+
+#include <base/Common.h>
+#include <base/stream/Serial.h>
+
+#include "../assert.h"
+#include "../pes.h"
+#include "../tcuif.h"
+
+using namespace m3;
+
+static constexpr size_t MSG_SIZE = 256;
+
+static uint8_t rbuf[64];
+
+int main() {
+    kernel::TCU::config_send(0, 0x1234, pe_id(PE::PE1), 0, nextlog2<MSG_SIZE>::val, 1);
+    size_t size = nextlog2<sizeof(rbuf)>::val;
+    uintptr_t rbuf_addr = reinterpret_cast<uintptr_t>(rbuf);
+    kernel::TCU::config_recv(1, rbuf_addr, size, size, TCU::NO_REPLIES);
+
+    Serial::get() << "Hello World from sender!\n";
+
+    uint64_t msg = 0xDEADBEEF;
+    while(1) {
+        // send message
+        ASSERT_EQ(kernel::TCU::send(0, &msg, sizeof(msg), 0x2222, 1), Errors::NONE);
+        msg++;
+
+        // wait for reply
+        const TCU::Message *rmsg;
+        while((rmsg = kernel::TCU::fetch_msg(1, rbuf_addr)) == nullptr)
+            ;
+        const uint64_t *data = reinterpret_cast<const uint64_t*>(rmsg->data);
+        Serial::get() << "Got reply: "
+            << "label=" << fmt(rmsg->label, "#x")
+            << ", payload=" << fmt(data[0], "#x") << "\n";
+
+        // ack reply
+        ASSERT_EQ(kernel::TCU::ack_msg(1, rbuf_addr, rmsg), Errors::NONE);
+    }
+    return 0;
+}
