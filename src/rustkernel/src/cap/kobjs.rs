cb5c3857e624a10d56f576aa078d72f8f579e3a1
Nils Asmussen <nils@os.inf.tu-dresden.de>
2020-06-30 09:27:52 +0200

    rustkernel: fixed print.

--- src/rustkernel/src/cap/kobjs.rs
+++ src/rustkernel/src/cap/kobjs.rs
@@ -677,7 +677,7 @@ impl fmt::Debug for EPObject {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(
             f,
-            "EPMask[vpe={}, ep={}, replies={}, pe={:?}]",
+            "EP[vpe={}, ep={}, replies={}, pe={:?}]",
             self.vpe().unwrap().id(),
             self.ep,
             self.replies,
