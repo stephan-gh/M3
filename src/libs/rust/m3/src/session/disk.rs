bc751f0ad82ede4ba0f343577008af33c6867a97
Nils Asmussen <nils@os.inf.tu-dresden.de>
2020-11-11 09:53:07 +0100

    m3fsrs: added copyright headers.

--- src/libs/rust/m3/src/session/disk.rs
+++ src/libs/rust/m3/src/session/disk.rs
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2015-2020, Nils Asmussen <nils@os.inf.tu-dresden.de>
+ * Copyright (C) 2019-2020, Tendsin Mende <tendsin@protonmail.com>
+ * Copyright (C) 2018, Sebastian Reimers <sebastian.reimers@mailbox.tu-dresden.de>
+ * Economic rights: Technische Universitaet Dresden (Germany)
+ *
+ * This file is part of M3 (Microkernel-based SysteM for Heterogeneous Manycores).
+ *
+ * M3 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * M3 is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License version 2 for more details.
+ */
+
 use crate::com::{MemGate, RecvGate, SendGate};
 use crate::errors::Error;
 use crate::int_enum;
--- src/server/m3fsrs/src/data/inode.rs
+++ src/server/m3fsrs/src/data/inode.rs
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2015-2020, Nils Asmussen <nils@os.inf.tu-dresden.de>
+ * Copyright (C) 2019-2020, Tendsin Mende <tendsin@protonmail.com>
+ * Economic rights: Technische Universitaet Dresden (Germany)
+ *
+ * This file is part of M3 (Microkernel-based SysteM for Heterogeneous Manycores).
+ *
+ * M3 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * M3 is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License version 2 for more details.
+ */
+
 use crate::buf::MetaBufferBlockRef;
 use crate::data::{
     BlockNo, Dev, Extent, FileMode, InodeNo, Time, INODE_DIR_COUNT, NUM_INODE_BYTES,
