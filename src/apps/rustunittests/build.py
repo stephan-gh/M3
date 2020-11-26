5aec94848a27f72ffcbe5540d5647fa17ba0f151
Nils Asmussen <nils@os.inf.tu-dresden.de>
2020-11-26 16:43:50 +0100

    rustunittests: don't build for M3_TARGET=hw M3_BUILD=debug.
    Unfortunately, the code is too large for the SPM.

--- src/apps/rustunittests/build.py
+++ src/apps/rustunittests/build.py
@@ -1,2 +1,7 @@
+import os
+
 def build(gen, env):
-    env.m3_rust_exe(gen, out = 'rustunittests')
+    if env['TGT'] != 'hw' or os.environ.get('M3_BUILD') != 'debug':
+        env.m3_rust_exe(gen, out = 'rustunittests')
+    else:
+        print("Warning: ignoring rustunittests")
