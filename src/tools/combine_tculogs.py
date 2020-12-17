ff075517266abec77c58c66dcb43f007810dfa92
Nils Asmussen <nils@os.inf.tu-dresden.de>
2020-12-17 17:58:20 +0100

    hw: copy TCU logs into run directory and added analysis tool.
    The combine_tculogs.py script allows to combine multiple TCU logs into
    a single log ordered by timestamp and colored according to the PM that
    created the TCU log entry.

---
+++ src/tools/combine_tculogs.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import os
+import re
+import sys
+
+if len(sys.argv) < 2:
+    print("Usage: {} <log-file>...".format(sys.argv[0]))
+    print("  The tool assumes that the file names match the pattern ^pm\d+-.*$")
+    sys.exit(1)
+
+events = []
+for file in sys.argv[1:]:
+    m = re.search("^pm(\d+)-.*$", os.path.basename(file))
+    pm = int(m[1])
+
+    f = open(file, 'r')
+    for line in f.readlines():
+        m = re.search("^\s*(\d+): Time:\s*(\d+), (.*)$", line)
+        if m:
+            events.append((pm, int(m[2]), m[3]))
+
+def sort_by_ts(event):
+    return event[1]
+
+colors = ["31", "32", "33", "34", "35", "36"]
+events.sort(key=sort_by_ts)
+for ev in events:
+    color = colors[ev[0] % len(colors)]
+    print("\x1B[0;{}m[PM{} @ {:>12}] {}\x1B[0m".format(color, ev[0], ev[1], ev[2]))
