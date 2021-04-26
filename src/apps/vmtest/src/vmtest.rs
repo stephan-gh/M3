9b64a99525b5c38b997af1134b92ab9e4b9292fc
Nils Asmussen <nils@os.inf.tu-dresden.de>
2021-04-26 10:10:18 +0200

    vmtest: use proper interrupt vector on hw.

--- src/apps/vmtest/src/vmtest.rs
+++ src/apps/vmtest/src/vmtest.rs
@@ -23,6 +23,7 @@ mod paging;
 
 use base::cell::{LazyStaticCell, StaticCell};
 use base::cfg;
+use base::envdata;
 use base::errors::{Code, Error};
 use base::io;
 use base::kif::{PageFlags, Perm};
@@ -408,7 +409,12 @@ pub extern "C" fn env_run() {
     isr::reg(isr::Vector::LOAD_PAGEFAULT.val, mmu_pf);
     isr::reg(isr::Vector::STORE_PAGEFAULT.val, mmu_pf);
     isr::reg(isr::Vector::SUPER_SW_IRQ.val, sw_irq);
-    isr::reg(isr::Vector::SUPER_EXT_IRQ.val, tcu_irq);
+    if envdata::get().platform == envdata::Platform::HW.val {
+        isr::reg(isr::Vector::MACH_EXT_IRQ.val, tcu_irq);
+    }
+    else {
+        isr::reg(isr::Vector::SUPER_EXT_IRQ.val, tcu_irq);
+    }
     isr::enable_irqs();
 
     log!(crate::LOG_DEF, "Triggering software IRQ...");
