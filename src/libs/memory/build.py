ac39f5ff9e85f3722e4bfb153f40e2c2d0c06a4f
Nils Asmussen <nils@os.inf.tu-dresden.de>
2021-05-26 09:12:20 +0200

    memory: disable LTO to prevent changes of symbol names.

--- src/libs/memory/build.py
+++ src/libs/memory/build.py
@@ -1,3 +1,5 @@
 def build(gen, env):
+    env = env.clone()
+    env.remove_flag('CXXFLAGS', '-flto')
     lib = env.static_lib(gen, out = 'libmemory', ins = env.glob('*.cc'))
     env.install(gen, env['LIBDIR'], lib)
