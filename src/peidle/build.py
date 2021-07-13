a17e6c5aa6860da9c1e7748cca5ca3b8a066ec47
Nils Asmussen <nils@os.inf.tu-dresden.de>
2021-07-13 10:48:41 +0200

    peidle: fixed link address.

--- src/peidle/build.py
+++ src/peidle/build.py
@@ -1,3 +1,3 @@
 def build(gen, env):
     if env['TGT'] == 'hw':
-        env.m3_rust_exe(gen, out = 'peidle', ldscript = 'pemux', libs = ['isr'])
+        env.m3_rust_exe(gen, out = 'peidle', ldscript = 'pemux', varAddr = False, libs = ['isr'])
