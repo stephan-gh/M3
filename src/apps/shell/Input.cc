a5a58de45d4805fa54fd9144561c275fe2f70321
Nils Asmussen <nils@os.inf.tu-dresden.de>
2021-09-15 10:34:13 +0200

    shell: added support for basic tab completion.

--- src/apps/shell/Input.cc
+++ src/apps/shell/Input.cc
@@ -15,12 +15,77 @@
  */
 
 #include <m3/stream/Standard.h>
+#include <m3/vfs/Dir.h>
 
 #include <ctype.h>
+#include <string>
+#include <vector>
 
 #include "Input.h"
 
 using namespace m3;
+
+static std::vector<std::string> get_completions(const char *line, size_t len, size_t *prefix_len) {
+    // determine prefix
+    size_t prefix_start = len;
+    while(prefix_start > 0 && !isspace(line[prefix_start - 1]))
+        prefix_start--;
+    // check whether we should complete binaries or a path
+    bool complete_bins = true;
+    for(ssize_t i = static_cast<ssize_t>(prefix_start) - 1; i >= 0; --i) {
+        if(line[i] == '|')
+            break;
+        if(!isspace(line[i])) {
+            complete_bins = false;
+            break;
+        }
+    }
+
+    const char *prefix = line + prefix_start;
+    *prefix_len = len - prefix_start;
+    std::vector<std::string> matches;
+
+    if(*prefix) {
+        Dir::Entry e;
+
+        if(complete_bins) {
+            try {
+                // we have no PATH, binary directory is hardcoded for now
+                Dir bin("/bin");
+                while(bin.readdir(e)) {
+                    if(strcmp(e.name, ".") == 0 || strcmp(e.name, "..") == 0)
+                        continue;
+                    if(strncmp(e.name, prefix, strlen(prefix)) == 0)
+                        matches.push_back(e.name);
+                }
+            }
+            catch(const Exception &) {
+                // ignore failures
+            }
+        }
+
+        // since we have no CWD yet, paths have to start with /
+        if(*prefix == '/') {
+            const char *lastdir = strrchr(prefix, '/');
+            const char *filename = lastdir + 1;
+            std::string dirname(prefix, 0, 1 + static_cast<size_t>(lastdir - prefix));
+            try {
+                Dir dir(dirname.c_str());
+                while(dir.readdir(e)) {
+                    if(strcmp(e.name, ".") == 0 || strcmp(e.name, "..") == 0)
+                        continue;
+                    if(strncmp(e.name, filename, strlen(filename)) == 0)
+                        matches.push_back(dirname + e.name);
+                }
+            }
+            catch(const Exception &) {
+                // ignore failures
+            }
+        }
+    }
+
+    return matches;
+}
 
 ssize_t Input::readline(char *buffer, size_t max) {
     size_t o = 0;
@@ -40,6 +105,27 @@ ssize_t Input::readline(char *buffer, size_t max) {
             continue;
 
         switch(c) {
+            case '\t': {
+                buffer[o] = '\0';
+                size_t prefix_len;
+                std::vector<std::string> matches = get_completions(buffer, o, &prefix_len);
+                if(matches.size() == 1) {
+                    for(char c : matches[0].substr(prefix_len)) {
+                        buffer[o++] = c;
+                        cout.write(c);
+                    }
+                    cout.flush();
+                }
+                else if(matches.size() > 0) {
+                    cout << "\n";
+                    for(auto &s : matches)
+                        cout << s.c_str() << " ";
+                    cout << "\n$ " << buffer;
+                    cout.flush();
+                }
+                break;
+            }
+
             // ^W
             case 0x17: {
                 for(; o > 0 && isspace(buffer[o - 1]); --o)
@@ -49,6 +135,7 @@ ssize_t Input::readline(char *buffer, size_t max) {
                 cout.flush();
                 break;
             }
+
             // backspace
             case 0x7F: {
                 if(o > 0) {
