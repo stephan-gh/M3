6d13f76b31aafee1ea00bef0f9db8d96591fddbf
Nils Asmussen <nils@os.inf.tu-dresden.de>
2021-12-03 15:07:16 +0100

    kecacc-xkcp: silence warnings.

--- src/libs/crypto/kecacc-xkcp/build.py
+++ src/libs/crypto/kecacc-xkcp/build.py
@@ -1,5 +1,8 @@
 def build(gen, env):
     env = env.clone()
-    env['CXXFLAGS'] += ['-std=c++17']  # C++17 is needed for template<auto ...>
+    env['CXXFLAGS'] += [
+        '-std=c++17',           # C++17 is needed for template<auto ...>
+        '-Wno-sign-conversion', # silence warnings
+    ]
     lib = env.static_lib(gen, out = 'libkecacc-xkcp', ins = ['kecacc-xkcp.cc', 'xkcp/KeccakP-1600-opt64.c'])
     env.install(gen, env['LIBDIR'], lib)
