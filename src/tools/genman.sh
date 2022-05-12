41d71b67a348f2b49ad20387cbc4666bc5bb2065
Nils Asmussen <nils@os.inf.tu-dresden.de>
2022-05-12 15:09:52 +0200

    bsdutils: remove binaries we don't really benefit from.
    After all, we have to (re)link all these binaries for every change in
    the libc or other dependencies.

--- src/tools/genman.sh
+++ src/tools/genman.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-progs="basename cat cp csplit cut date dd dirname du echo expr factor false find fmt head join ln ls"
-progs="$progs mkdir mktemp mv nl paste pathchk printenv printf pwd rm rmdir sleep split stat sync"
-progs="$progs tail tee test touch tr true tsort uniq wc yes"
+progs="basename cat cp cut date dd dirname du find head ln ls"
+progs="$progs mkdir mktemp mv printenv printf pwd rm rmdir sleep stat sync"
+progs="$progs tail tee test tr uniq wc"
 
 mkdir -p src/fs/default/man
 
