8db978f6f21d8bf7bbfa0c08a213ffc8d0e01f96
Nils Asmussen <nils@os.inf.tu-dresden.de>
2022-06-27 15:57:28 +0200

    gem5: fixed bugs in stream accelerators.

--- config/demo.py
+++ config/demo.py
@@ -41,7 +41,7 @@ for i in range(0, len(accs)):
                            no=num_tiles + i,
                            accel=accs[i],
                            memTile=mem_tile,
-                           spmsize='2MB',
+                           spmsize='4MB',
                            epCount=num_eps)
     tiles.append(tile)
 
