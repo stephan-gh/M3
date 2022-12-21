4589927d9e82322c338a21658c72617e5c744261
Nils Asmussen <nils@os.inf.tu-dresden.de>
2022-12-21 15:18:51 +0100

    arm: always ensure that Entry.S is included via --whole-archive.

--- src/apps/ruststandalone/stdasender/build.py
+++ src/apps/ruststandalone/stdasender/build.py
@@ -1,4 +1,9 @@
 def build(gen, env):
+    env = env.clone()
+
+    if env['ISA'] == 'arm':
+        env['LINKFLAGS'] += ['-Wl,--whole-archive', '-lisr', '-Wl,--no-whole-archive']
+
     env.m3_rust_exe(
         gen,
         out = 'stdasender',
--- src/tilemux/build.py
+++ src/tilemux/build.py
@@ -2,6 +2,8 @@ def build(gen, env):
     env = env.clone()
 
     env['LINKFLAGS'] += ['-nostartfiles']
+    if env['ISA'] == 'arm':
+        env['LINKFLAGS'] += ['-Wl,--whole-archive', '-lisr', '-Wl,--no-whole-archive']
 
     entry_file = 'src/arch/' + env['ISA'] + '/Entry.S'
     entry = env.asm(gen, out = entry_file[:-2] + '.o', ins = [entry_file])
