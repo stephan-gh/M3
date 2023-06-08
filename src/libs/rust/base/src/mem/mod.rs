00397577208a76aea83dd8becb529d961dd609f5
Nils Asmussen <nils@os.inf.tu-dresden.de>
2023-06-08 10:46:05 +0200

    librustbase: use macro for the generic part of the PrimInt impl.

--- src/libs/rust/base/src/mem/mod.rs
+++ src/libs/rust/base/src/mem/mod.rs
@@ -28,3 +28,258 @@ pub use self::globaddr::GlobAddr;
 pub use self::map::MemMap;
 pub use self::virtaddr::{VirtAddr, VirtAddrRaw};
 pub use core::mem::{align_of, align_of_val, forget, replace, size_of, size_of_val, MaybeUninit};
+
+#[macro_export]
+macro_rules! impl_prim_int {
+    ($T:ty, $R:ty) => {
+        impl num_traits::Saturating for $T {
+            fn saturating_add(self, v: Self) -> Self {
+                Self(self.0.saturating_add(v.0))
+            }
+
+            fn saturating_sub(self, v: Self) -> Self {
+                Self(self.0.saturating_sub(v.0))
+            }
+        }
+
+        impl core::ops::Div for $T {
+            type Output = Self;
+
+            fn div(self, rhs: Self) -> Self::Output {
+                Self(self.0 / rhs.0)
+            }
+        }
+
+        impl num_traits::CheckedDiv for $T {
+            fn checked_div(&self, v: &Self) -> Option<Self> {
+                self.0.checked_div(v.0).map(|v| Self(v))
+            }
+        }
+
+        impl core::ops::Rem for $T {
+            type Output = Self;
+
+            fn rem(self, rhs: Self) -> Self::Output {
+                Self(self.0 % rhs.0)
+            }
+        }
+
+        impl core::ops::Mul for $T {
+            type Output = Self;
+
+            fn mul(self, rhs: Self) -> Self::Output {
+                Self(self.0 * rhs.0)
+            }
+        }
+
+        impl num_traits::CheckedMul for $T {
+            fn checked_mul(&self, v: &Self) -> Option<Self> {
+                self.0.checked_mul(v.0).map(|v| Self(v))
+            }
+        }
+
+        impl core::ops::Add<$T> for $T {
+            type Output = Self;
+
+            fn add(self, rhs: Self) -> Self::Output {
+                Self(self.0 + rhs.0)
+            }
+        }
+
+        impl num_traits::CheckedAdd for $T {
+            fn checked_add(&self, v: &Self) -> Option<Self> {
+                self.0.checked_add(v.0).map(|v| Self(v))
+            }
+        }
+
+        impl core::ops::Sub<$T> for $T {
+            type Output = Self;
+
+            fn sub(self, rhs: Self) -> Self::Output {
+                Self(self.0 - rhs.0)
+            }
+        }
+
+        impl num_traits::CheckedSub for $T {
+            fn checked_sub(&self, v: &Self) -> Option<Self> {
+                self.0.checked_sub(v.0).map(|v| Self(v))
+            }
+        }
+
+        impl core::ops::Shr<usize> for $T {
+            type Output = Self;
+
+            fn shr(self, rhs: usize) -> Self::Output {
+                Self(self.0 >> rhs)
+            }
+        }
+
+        impl core::ops::Shl<usize> for $T {
+            type Output = Self;
+
+            fn shl(self, rhs: usize) -> Self::Output {
+                Self(self.0 << rhs)
+            }
+        }
+
+        impl core::ops::BitXor<Self> for $T {
+            type Output = Self;
+
+            fn bitxor(self, rhs: Self) -> Self::Output {
+                Self(self.0 ^ rhs.0)
+            }
+        }
+
+        impl core::ops::BitOr<Self> for $T {
+            type Output = Self;
+
+            fn bitor(self, rhs: Self) -> Self::Output {
+                Self(self.0 | rhs.0)
+            }
+        }
+
+        impl core::ops::BitAnd<Self> for $T {
+            type Output = Self;
+
+            fn bitand(self, rhs: Self) -> Self::Output {
+                Self(self.0 & rhs.0)
+            }
+        }
+
+        impl core::ops::Not for $T {
+            type Output = Self;
+
+            fn not(self) -> Self::Output {
+                Self(!self.0)
+            }
+        }
+
+        impl num_traits::Bounded for $T {
+            fn min_value() -> Self {
+                Self(<$R>::min_value())
+            }
+
+            fn max_value() -> Self {
+                Self(<$R>::max_value())
+            }
+        }
+
+        impl num_traits::ToPrimitive for $T {
+            fn to_i64(&self) -> Option<i64> {
+                self.0.to_i64()
+            }
+
+            fn to_u64(&self) -> Option<u64> {
+                self.0.to_u64()
+            }
+        }
+
+        impl num_traits::NumCast for $T {
+            fn from<T: num_traits::ToPrimitive>(n: T) -> Option<Self> {
+                <$R as num_traits::NumCast>::from(n).map(|v| Self(v))
+            }
+        }
+
+        impl num_traits::Zero for $T {
+            fn zero() -> Self {
+                Self(0)
+            }
+
+            fn is_zero(&self) -> bool {
+                self.as_raw() == 0
+            }
+        }
+
+        impl num_traits::One for $T {
+            fn one() -> Self {
+                Self(1)
+            }
+        }
+
+        impl num_traits::Num for $T {
+            type FromStrRadixErr = core::num::ParseIntError;
+
+            fn from_str_radix(str: &str, radix: u32) -> Result<Self, Self::FromStrRadixErr> {
+                <$R>::from_str_radix(str, radix).map(|v| Self(v))
+            }
+        }
+
+        impl num_traits::PrimInt for $T {
+            fn count_ones(self) -> u32 {
+                self.0.count_ones()
+            }
+
+            fn count_zeros(self) -> u32 {
+                self.0.count_zeros()
+            }
+
+            fn leading_ones(self) -> u32 {
+                self.0.leading_ones()
+            }
+
+            fn leading_zeros(self) -> u32 {
+                self.0.leading_zeros()
+            }
+
+            fn trailing_ones(self) -> u32 {
+                self.0.trailing_ones()
+            }
+
+            fn trailing_zeros(self) -> u32 {
+                self.0.trailing_zeros()
+            }
+
+            fn rotate_left(self, n: u32) -> Self {
+                Self(self.0.rotate_left(n))
+            }
+
+            fn rotate_right(self, n: u32) -> Self {
+                Self(self.0.rotate_right(n))
+            }
+
+            fn signed_shl(self, n: u32) -> Self {
+                Self(self.0.signed_shl(n))
+            }
+
+            fn signed_shr(self, n: u32) -> Self {
+                Self(self.0.signed_shr(n))
+            }
+
+            fn unsigned_shl(self, n: u32) -> Self {
+                Self(self.0.unsigned_shl(n))
+            }
+
+            fn unsigned_shr(self, n: u32) -> Self {
+                Self(self.0.unsigned_shr(n))
+            }
+
+            fn swap_bytes(self) -> Self {
+                Self(self.0.swap_bytes())
+            }
+
+            fn reverse_bits(self) -> Self {
+                Self(self.0.reverse_bits())
+            }
+
+            fn from_be(x: Self) -> Self {
+                Self(<$R as num_traits::PrimInt>::from_be(x.0))
+            }
+
+            fn from_le(x: Self) -> Self {
+                Self(<$R as num_traits::PrimInt>::from_le(x.0))
+            }
+
+            fn to_be(self) -> Self {
+                Self(self.0.to_be())
+            }
+
+            fn to_le(self) -> Self {
+                Self(self.0.to_le())
+            }
+
+            fn pow(self, exp: u32) -> Self {
+                Self(self.0.pow(exp))
+            }
+        }
+    };
+}
--- src/libs/rust/base/src/mem/virtaddr.rs
+++ src/libs/rust/base/src/mem/virtaddr.rs
@@ -14,10 +14,10 @@
  */
 
 use core::fmt;
-use core::num::ParseIntError;
 use core::ops;
 
 use crate::goff;
+use crate::impl_prim_int;
 use crate::serialize::{Deserialize, Serialize};
 
 pub type VirtAddrRaw = u64;
@@ -89,52 +89,6 @@ impl fmt::Debug for VirtAddr {
     }
 }
 
-impl num_traits::Saturating for VirtAddr {
-    fn saturating_add(self, v: Self) -> Self {
-        Self(self.0.saturating_add(v.0))
-    }
-
-    fn saturating_sub(self, v: Self) -> Self {
-        Self(self.0.saturating_sub(v.0))
-    }
-}
-
-impl ops::Div for VirtAddr {
-    type Output = Self;
-
-    fn div(self, rhs: Self) -> Self::Output {
-        Self(self.0 / rhs.0)
-    }
-}
-
-impl num_traits::CheckedDiv for VirtAddr {
-    fn checked_div(&self, v: &Self) -> Option<Self> {
-        self.0.checked_div(v.0).map(|v| Self(v))
-    }
-}
-
-impl ops::Rem for VirtAddr {
-    type Output = Self;
-
-    fn rem(self, rhs: Self) -> Self::Output {
-        Self(self.0 % rhs.0)
-    }
-}
-
-impl ops::Mul for VirtAddr {
-    type Output = Self;
-
-    fn mul(self, rhs: Self) -> Self::Output {
-        Self(self.0 * rhs.0)
-    }
-}
-
-impl num_traits::CheckedMul for VirtAddr {
-    fn checked_mul(&self, v: &Self) -> Option<Self> {
-        self.0.checked_mul(v.0).map(|v| Self(v))
-    }
-}
-
 impl ops::Add<usize> for VirtAddr {
     type Output = Self;
 
@@ -151,23 +105,9 @@ impl ops::Add<goff> for VirtAddr {
     }
 }
 
-impl ops::Add<VirtAddr> for VirtAddr {
-    type Output = Self;
-
-    fn add(self, rhs: Self) -> Self::Output {
-        Self(self.0 + rhs.0)
-    }
-}
-
 impl ops::AddAssign<usize> for VirtAddr {
     fn add_assign(&mut self, rhs: usize) {
         self.0 += rhs as VirtAddrRaw;
-    }
-}
-
-impl num_traits::CheckedAdd for VirtAddr {
-    fn checked_add(&self, v: &Self) -> Option<Self> {
-        self.0.checked_add(v.0).map(|v| Self(v))
     }
 }
 
@@ -179,192 +119,4 @@ impl ops::Sub<usize> for VirtAddr {
     }
 }
 
-impl ops::Sub<VirtAddr> for VirtAddr {
-    type Output = Self;
-
-    fn sub(self, rhs: Self) -> Self::Output {
-        Self(self.0 - rhs.0)
-    }
-}
-
-impl num_traits::CheckedSub for VirtAddr {
-    fn checked_sub(&self, v: &Self) -> Option<Self> {
-        self.0.checked_sub(v.0).map(|v| Self(v))
-    }
-}
-
-impl ops::Shr<usize> for VirtAddr {
-    type Output = Self;
-
-    fn shr(self, rhs: usize) -> Self::Output {
-        Self(self.0 >> rhs)
-    }
-}
-
-impl ops::Shl<usize> for VirtAddr {
-    type Output = Self;
-
-    fn shl(self, rhs: usize) -> Self::Output {
-        Self(self.0 << rhs)
-    }
-}
-
-impl ops::BitXor<Self> for VirtAddr {
-    type Output = Self;
-
-    fn bitxor(self, rhs: Self) -> Self::Output {
-        Self(self.0 ^ rhs.0)
-    }
-}
-
-impl ops::BitOr<Self> for VirtAddr {
-    type Output = Self;
-
-    fn bitor(self, rhs: Self) -> Self::Output {
-        Self(self.0 | rhs.0)
-    }
-}
-
-impl ops::BitAnd<Self> for VirtAddr {
-    type Output = Self;
-
-    fn bitand(self, rhs: Self) -> Self::Output {
-        Self(self.0 & rhs.0)
-    }
-}
-
-impl ops::Not for VirtAddr {
-    type Output = Self;
-
-    fn not(self) -> Self::Output {
-        Self(!self.0)
-    }
-}
-
-impl num_traits::Bounded for VirtAddr {
-    fn min_value() -> Self {
-        Self(VirtAddrRaw::min_value())
-    }
-
-    fn max_value() -> Self {
-        Self(VirtAddrRaw::max_value())
-    }
-}
-
-impl num_traits::ToPrimitive for VirtAddr {
-    fn to_i64(&self) -> Option<i64> {
-        self.0.to_i64()
-    }
-
-    fn to_u64(&self) -> Option<u64> {
-        self.0.to_u64()
-    }
-}
-
-impl num_traits::NumCast for VirtAddr {
-    fn from<T: num_traits::ToPrimitive>(n: T) -> Option<Self> {
-        <VirtAddrRaw as num_traits::NumCast>::from(n).map(|v| Self(v))
-    }
-}
-
-impl num_traits::Zero for VirtAddr {
-    fn zero() -> Self {
-        Self(0)
-    }
-
-    fn is_zero(&self) -> bool {
-        self.is_null()
-    }
-}
-
-impl num_traits::One for VirtAddr {
-    fn one() -> Self {
-        Self(1)
-    }
-}
-
-impl num_traits::Num for VirtAddr {
-    type FromStrRadixErr = ParseIntError;
-
-    fn from_str_radix(str: &str, radix: u32) -> Result<Self, Self::FromStrRadixErr> {
-        VirtAddrRaw::from_str_radix(str, radix).map(|v| Self(v))
-    }
-}
-
-impl num_traits::PrimInt for VirtAddr {
-    fn count_ones(self) -> u32 {
-        self.0.count_ones()
-    }
-
-    fn count_zeros(self) -> u32 {
-        self.0.count_zeros()
-    }
-
-    fn leading_ones(self) -> u32 {
-        self.0.leading_ones()
-    }
-
-    fn leading_zeros(self) -> u32 {
-        self.0.leading_zeros()
-    }
-
-    fn trailing_ones(self) -> u32 {
-        self.0.trailing_ones()
-    }
-
-    fn trailing_zeros(self) -> u32 {
-        self.0.trailing_zeros()
-    }
-
-    fn rotate_left(self, n: u32) -> Self {
-        Self(self.0.rotate_left(n))
-    }
-
-    fn rotate_right(self, n: u32) -> Self {
-        Self(self.0.rotate_right(n))
-    }
-
-    fn signed_shl(self, n: u32) -> Self {
-        Self(self.0.signed_shl(n))
-    }
-
-    fn signed_shr(self, n: u32) -> Self {
-        Self(self.0.signed_shr(n))
-    }
-
-    fn unsigned_shl(self, n: u32) -> Self {
-        Self(self.0.unsigned_shl(n))
-    }
-
-    fn unsigned_shr(self, n: u32) -> Self {
-        Self(self.0.unsigned_shr(n))
-    }
-
-    fn swap_bytes(self) -> Self {
-        Self(self.0.swap_bytes())
-    }
-
-    fn reverse_bits(self) -> Self {
-        Self(self.0.reverse_bits())
-    }
-
-    fn from_be(x: Self) -> Self {
-        Self(<VirtAddrRaw as num_traits::PrimInt>::from_be(x.0))
-    }
-
-    fn from_le(x: Self) -> Self {
-        Self(<VirtAddrRaw as num_traits::PrimInt>::from_le(x.0))
-    }
-
-    fn to_be(self) -> Self {
-        Self(self.0.to_be())
-    }
-
-    fn to_le(self) -> Self {
-        Self(self.0.to_le())
-    }
-
-    fn pow(self, exp: u32) -> Self {
-        Self(self.0.pow(exp))
-    }
-}
+impl_prim_int!(VirtAddr, VirtAddrRaw);
