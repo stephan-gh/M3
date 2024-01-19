db4e09e7efb8a3375cb125e9fb7a30d51b12a5ba
Nils Asmussen <nils@os.inf.tu-dresden.de>
2024-01-19 10:45:36 +0100

    kecacc: replace subdirectory with submodule.
    As gem5's build system is apparently not always able to recognize
    changes in the symlinked files/directories (and this approach had the
    downside to only work if the gem5 repo is a submodule within the M³
    repo), I've now replaced it with a submodule that is both used in M³ and
    gem5.

--- src/libs/crypto/kecacc-xkcp/build.py
+++
@@ -1,9 +0,0 @@
-def build(gen, env):
-    env = env.clone()
-    env['CXXFLAGS'] += [
-        '-std=c++17',           # C++17 is needed for template<auto ...>
-        '-Wno-sign-conversion',  # silence warnings
-    ]
-    files = ['kecacc-xkcp.cc', 'xkcp/KeccakP-1600-opt64.c']
-    lib = env.static_lib(gen, out='kecacc-xkcp', ins=files)
-    env.install(gen, env['LIBDIR'], lib)
